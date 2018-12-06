c4fc546fad7b9a2b20e8b6b98c56fcda6274d201
Lexxandrik <34407150+Lexxandrik@users.noreply.github.com>
2018-12-06 10:21:14 +1000

    Update rivals_analysis_rutaxi.py

--- rivals_analysis_rutaxi.py
+++ rivals_analysis_rutaxi.py
@@ -11,10 +11,10 @@ driver = webdriver.Firefox()
 
 driver.get(url)
 
-addr_from = driver.find_element_by_xpath("//div[@id='routers'][0]/input[contains(@class,'nostyle str_obj xdsoft_input')]")
-addr_to = driver.find_element_by_xpath("//div[@id='routers'][1]/input[contains(@class,'nostyle str_obj xdsoft_input')]")
-addr_from_house = 
-addr_to_house = 
+addr_from = driver.find_element_by_xpath("//div[@id='routers']/input[contains(@class,'nostyle str_obj xdsoft_input')][0]")
+addr_to = driver.find_element_by_xpath("//div[@id='routers']/input[contains(@class,'nostyle str_obj xdsoft_input')][1]")
+addr_from_house = driver.find_element_by_xpath("//div[@id='routers']/input[contains(@class,'nostyle text-center str_h house')][0]")
+addr_to_house = driver.find_element_by_xpath("//div[@id='routers']/input[contains(@class,'nostyle text-center str_h house')][1]")
 sleep(5)
 
 streets =[]
